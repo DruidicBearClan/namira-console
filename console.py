887fbe7200439be55b6b453dcf58cfdd71a6bf30
Linnaeus Lindquist <l.lindquist@live.nl>
2021-01-29 23:24:57 +0100

    changed file name and listed all kivy libraries

--- console.py
+++
@@ -1,6 +0,0 @@
-from tkinter import *
-
-root = Tk()
-root.title("label-test") # set window title
-root.geometry("200x300") # set window size note : x is not *
-root.resizable(width=True, height=False) # set whether the window can be lengthened / wide, False immutable, True variable, default is True
