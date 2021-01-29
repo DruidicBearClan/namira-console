977fbf10567b399c3183b27059a918d3b40e3a68
Linnaeus Lindquist <l.lindquist@live.nl>
2021-01-29 23:47:53 +0100

    fixed indenting

--- console.py
+++ console.py
@@ -4,12 +4,9 @@ kivy.require('2.0.0') #Current kivy version
 from kivy.app import App
 from kivy.uix.label import Label
 
-
 class MyApp(App):
-
     def build(self):
         return Label(text='Hello world')
-
 
 if __name__ == '__main__':
     MyApp().run()
